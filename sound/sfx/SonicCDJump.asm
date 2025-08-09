caffcb7b1ead1b32f8f9d50aec2c53a8a9f41125
Mildanner <cianocriativo408@gmail.com>
2025-08-09 13:09:29 -0300

    Create SonicCDJump.asm for Sonic CD Jump Sound Effect

---
+++ sound/sfx/SonicCDJump.asm
@@ -0,0 +1,10 @@
+; Sonic CD Jump Sound Effect
+
+; This is the assembly code for the Sonic CD jump sound effect.
+
+SonicCDJump:
+    ; Code to execute the jump sound
+    ; Add your assembly instructions here
+
+    
+    ; End of Sonic CD Jump sound effect
